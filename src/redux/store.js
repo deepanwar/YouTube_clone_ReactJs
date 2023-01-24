1cb60029cec4c9f14a1eb16009ba1bed41ab2410
Deepak Kumar <93703027+deepanwar@users.noreply.github.com>
2023-01-24 16:39:28 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,17 +12,3 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(youtubeApi.middleware),
 });
-
-// import { configureStore } from "@reduxjs/toolkit";
-
-// import playerReducer from "./features/playerSlice";
-// import { shazamCoreApi } from "./services/shazamCore";
-
-// export const store = configureStore({
-//   reducer: {
-//     player: playerReducer,
-//     [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware().concat(shazamCoreApi.middleware),
-// });
